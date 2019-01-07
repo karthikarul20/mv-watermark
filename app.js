d2bb095e34f9555c9ea31e18c44f8033f4ccee6f
karthikarul20 <karthik@yuvohub.com>
2019-01-07 23:39:37 +0800

    PDF Viewer example

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var app = angular.module('plunker', []);
+var app = angular.module('plunker', ['pdfjsViewer']);
 
-app.controller('MainCtrl', function($scope) {
+app.controller('MainCtrl', function($scope, $http) {
   $scope.test = function(){
     console.log("Test Success");
   };
+
+  $scope.data = null; // this is loaded async
+
+  $scope.pdf = {
+    src: 'example.pdf',
+};
+
 });
